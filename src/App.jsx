e7459e869ab9ab174e1cdacbb623d0f334b61529
ClaPalo <claudioclaudio91@gmail.com>
2024-09-02 19:37:41 -0500

    Added name and surname to sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { Container } from "react-bootstrap";
-import { Route, Routes } from "react-router-dom";
-import AuthRoute from "./components/AuthRoute";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import PasswordReset from "./pages/PasswordReset";
-import Register from "./pages/Register";
-import UpdatePassword from "./pages/UpdatePassword";
-import NavBar from "./components/Navbar";
+import { Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import AuthRoute from './components/AuthRoute'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import PasswordReset from './pages/PasswordReset'
+import Register from './pages/Register'
+import UpdatePassword from './pages/UpdatePassword'
+import NavBar from './components/Navbar'
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Routes>
-            <Route element={<AuthRoute />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-            </Route>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/passwordreset" element={<PasswordReset />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
-          </Routes>
-        </div>
-      </Container>
-    </>
-  );
-};
+    return (
+        <>
+            <NavBar />
+            <Container
+                className="d-flex align-items-center justify-content-center"
+                style={{ minHeight: '100vh' }}
+            >
+                <div className="w-100" style={{ maxWidth: '400px' }}>
+                    <Routes>
+                        <Route element={<AuthRoute />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/home" element={<Home />} />
+                        </Route>
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route
+                            path="/passwordreset"
+                            element={<PasswordReset />}
+                        />
+                        <Route
+                            path="/update-password"
+                            element={<UpdatePassword />}
+                        />
+                    </Routes>
+                </div>
+            </Container>
+        </>
+    )
+}
 
-export default App;
+export default App
