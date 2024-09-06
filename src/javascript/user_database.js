a3e1dd502ce463c65c95f2a64b4cadb5e561ccd1
ClaPalo <claudioclaudio91@gmail.com>
2024-09-06 18:35:13 -0500

    Deleted mail getter

--- src/javascript/user_database.js
+++ src/javascript/user_database.js
@@ -4,19 +4,10 @@ import { supabase } from '../supabaseClient'
  * Get user name and surname, given the id
  */
 export async function getUserName(id) {
-	const { data, error } = await supabase
-		.from('Users')
-		.select('name, surname')
-		.eq('id', id)
+    const { data, error } = await supabase
+        .from('Users')
+        .select('name, surname')
+        .eq('id', id)
 
-	return data
-}
-
-export async function getUserEmail(id) {
-	const { data, error } = await supabase
-		.from('Users')
-		.select('email')
-		.eq('id', id)
-
-	return data
+    return data
 }
