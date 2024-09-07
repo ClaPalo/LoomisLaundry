28bdb4c06fd978d4e646cbc0ae9a2ba4766fb4fd
ClaPalo <claudioclaudio91@gmail.com>
2024-09-06 21:35:34 -0500

    Fixed link to update password

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -13,7 +13,7 @@ const signOut = () => supabase.auth.signOut()
 
 const passwordReset = (email) =>
     supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'https://www.paloscia.com/LoomisLaundry/update-password',
+        redirectTo: 'https://www.paloscia.com/LoomisLaundry/#/update-password',
     })
 
 const updatePassword = (updatedPassword) =>
